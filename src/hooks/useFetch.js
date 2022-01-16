6ce1c332742bc82f49561195612f7b67bfeb4d82
q4q0 <s201830040@kfupm.edu.sa>
2022-01-16 06:55:55 +0300

    add error handler

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,14 +3,24 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
-
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       setIsPending(true);
-      const response = await fetch(url);
-      const json = await response.json();
-      setIsPending(false);
-      setData(json);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const json = await res.json();
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        setError(`Something went wrong`);
+        console.log(err);
+        setIsPending(false);
+      }
     };
     fetchData();
   }, [url]);
